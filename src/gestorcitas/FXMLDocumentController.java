1640e8c3f82330125ade6c2356cf92243b1d82cf
lisas <lisas@192.168.1.136>
2019-04-07 20:12:35 +0200

    Recién creado

--- src/gestorcitas/FXMLDocumentController.java
+++ src/gestorcitas/FXMLDocumentController.java
@@ -10,7 +10,9 @@ import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 
 /**
  *
@@ -18,10 +20,12 @@ import javafx.scene.control.Label;
  */
 public class FXMLDocumentController implements Initializable {
     
-    @FXML
     private Label label;
-    
     @FXML
+    private TextField password;
+    @FXML
+    private TextField usuario;
+    
     private void handleButtonAction(ActionEvent event) {
         System.out.println("You clicked me!");
         label.setText("Hello World!");
@@ -31,5 +35,9 @@ public class FXMLDocumentController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void logIn(ActionEvent event) {
+    }
     
 }
