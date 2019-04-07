25159cdb107064fcee47f9251451e1b4b148133b
thepu <thepu@DESKTOP-0R8EDGL>
2019-04-07 19:36:17 +0200

    He añadido un comentario en el fichero

--- src/gestorcitas/GestorCitas.java
+++ src/gestorcitas/GestorCitas.java
@@ -22,7 +22,7 @@ public class GestorCitas extends Application {
         Parent root = FXMLLoader.load(getClass().getResource("FXMLDocument.fxml"));
         
         Scene scene = new Scene(root);
-        
+        System.out.println("Hola desde GIT");
         stage.setScene(scene);
         stage.show();
     }
